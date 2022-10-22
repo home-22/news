6332f311cd68f7a55f3ff138ff6d1b49599e1b23
home-22 <armin.jasarevic.25@gmail.com>
2022-10-22 21:47:31 +0200

    News App_UI

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:news/page/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App Ui',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(cardColor: Colors.grey),
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+    );
+  }
+}
