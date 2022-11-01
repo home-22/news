76852186dde00c97f595b1d5590475af2c000c6b
home-22 <armin.jasarevic.25@gmail.com>
2022-11-01 22:40:09 +0100

    news

--- lib/widgets/Categories.dart
+++ lib/widgets/Categories.dart
@@ -1,36 +1,97 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Categories extends StatelessWidget {
   const Categories({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Categories'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextFormField(),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  child: Text('For You'),
-                ),
-                Spacer(),
-                CircleAvatar(
-                  radius: 10,
-                )
-              ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.grey,
             ),
-          ],
+          ),
+          centerTitle: true,
+          title: const Text(
+            'Categories',
+            style: TextStyle(color: Colors.grey),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.shade300),
+                  child: Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Text(
+                        'Search...',
+                        style: TextStyle(color: Colors.blue),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  children: [
+                    for (int i = 1; i < 10; i++)
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        height: 50,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white70,
+                        ),
+                        child: Row(
+                          children: [
+                            const Text(
+                              'For You',
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                            const Spacer(),
+                            InkWell(
+                              onTap: () {},
+                              child: const Icon(
+                                CupertinoIcons.check_mark_circled_solid,
+                                size: 30,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
